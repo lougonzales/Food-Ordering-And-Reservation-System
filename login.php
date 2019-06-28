7e2e4a20a293da59a3898ad2d0c6dfc5a3554678
pathabaan <habaan_patricia@yahoo.com>
2019-06-29 01:20:23 +0800

    pepe

--- login.php
+++ login.php
@@ -55,7 +55,7 @@ else{
       <form method="post" action="routers/router.php" class="login-form" id="form">
         <div class="row">
           <div class="input-field col s12 center">
-            <p class="center login-form-text">Login for Food Ordering System</p>
+            <p class="center login-form-text">Login for Food Ordering & Reservation System</p>
           </div>
         </div>
         <div class="row margin">
---
+++ user_view_menu.php
@@ -0,0 +1,321 @@
+<html>
+    <head>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+         <!-- Bootstrap JS -->
+         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </head>
+    
+    <body style="background-color: #FFA07A">
+ <!-- 3 Column with Picture -->
+ <div class="container mx-auto">
+            <div class="jumbotrons">
+                <h1 class="display-4 flex text-center">--MENU--</h1>
+                <hr class="my-4">
+            </div>
+            <div class="row">
+                <div class="col-sm-3 mb-3">    
+                    <div class="card">
+                        <img src="menu\chickenpopswithrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 60.00</h5>
+                               <p class="card-text">Chicken Pops with Rice</p>
+                           
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3">    
+                    <div class="card">
+                        <img src="menu\chickenskinwithrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 65.00</h5>
+                               <p class="card-text">Chicken Skin with Rice</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3">     
+                    <div class="card">
+                        <img src="menu\chickenwithspaghettifriesandrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 85.00</h5>
+                            <p class="card-text">Chicken with Spaghetti, Fries and Rice </p>
+                              
+                        </div>
+                    </div>
+               </div>
+               <div class="col-sm-3 mb-3">     
+                    <div class="card">
+                        <img src="menu\2pcchickenwithrice.png"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 115.00</h5>
+                            <p class="card-text">2 Pieces Chicken with Rice (unlimited gravy)</p>
+                              
+                        </div>
+                    </div>
+               </div>
+            </div>
+
+           <!-- 4 Column with Picture -->
+          <div class="container mx-auto">
+            <div class="row">
+                <div class="col-sm-3 mb-3">    
+                    <div class="card">
+                        <img src="menu\liempowithrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 75.00</h5>
+                               <p class="card-text">Liempo with Rice</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3">    
+                    <div class="card">
+                        <img src="menu\chickencordonbleuwithrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 65.00</h5>
+                               <p class="card-text">Chicken Cordon Bleu with Rice</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3">     
+                    <div class="card">
+                        <img src="menu\buffalowingswithrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php. 70.00</h5>
+                            <p class="card-text">Buffalo wings with Rice</p>
+                              
+                        </div>
+                    </div>
+               </div>
+               <div class="col-sm-3 mb-3">     
+                    <div class="card">
+                        <img src="menu\bagnetwithrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 80.00</h5>
+                            <p class="card-text">Bagnet with Rice</p>
+                              
+                        </div>
+                    </div>
+               </div>
+            </div>
+
+            <!-- 5 Column with Picture -->
+          <div class="container mx-auto">
+            <div class="jumbotrons">
+                <h1 class="display-4">DOUBLE:</h1>
+                <hr class="my-4">
+            </div>
+            <div class="row mx-auto">
+                <div class="col-sm-3 mb-3 mx-auto">    
+                    <div class="card">
+                        <img src="menu\chickenwithrice.png"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 60.00</h5>
+                               <p class="card-text">D1. Chicken with Rice</p>                                
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3 mx-auto">    
+                    <div class="card">
+                        <img src="menu\chickenwithspaghetti.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 65.00</h5>
+                               <p class="card-text">D2. Chicken with Spaghetti</p>        
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3 mx-auto">     
+                    <div class="card">
+                        <img src="menu\chickenwithfries.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 65.00</h5>
+                            <p class="card-text">D3. Chicken with Fries</p>
+                              
+                        </div>
+                    </div>
+               </div>
+             </div>
+        
+             
+            <!-- 6 Column with Picture -->
+          <div class="container mx-auto">
+            <div class="jumbotrons">
+                <h1 class="display-4">TRIO:</h1>
+                <hr class="my-4">
+            </div>
+            <div class="row mx-auto">
+                <div class="col-sm-3 mb-3 mx-auto">    
+                    <div class="card">
+                        <img src="menu\chickenwithspaghettiandrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 75.00</h5>
+                               <p class="card-text">T1. Chicken with Spaghetti and Rice</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3 mx-auto">    
+                    <div class="card">
+                        <img src="menu\chickenwithfriesandrice.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 75.00</h5>
+                               <p class="card-text">T2. Chicken with Fries and Rice</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3 mx-auto">     
+                    <div class="card">
+                        <img src="menu\chickenwithspagandfries.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 75.00</h5>
+                            <p class="card-text">T3. Chicken with Spaghetti and Fries</p>
+                              
+                        </div>
+                    </div>
+               </div>
+             </div>
+
+             
+            <!-- 7 Column with Picture -->
+          <div class="container mx-auto">
+            <div class="jumbotrons">
+                <h1 class="display-4">SIZZLING MEALS:</h1>
+                <hr class="my-4">
+            </div>
+            <div class="row mx-auto">
+                <div class="col-sm-3 mb-3 mx-auto">    
+                    <div class="card">
+                        <img src="menu\sizzlingporksisig.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 70.00</h5>
+                               <p class="card-text">S1. Sizzling Pork Sisig</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3 mx-auto">    
+                    <div class="card">
+                        <img src="menu\sizzlingtuna.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 75.00</h5>
+                               <p class="card-text">S2. Sizzling Tuna</p>                                 
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3 mx-auto">     
+                    <div class="card">
+                        <img src="menu\sizzlingbagnet.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 100.00</h5>
+                            <p class="card-text">S3. Sizzling Bagnet</p>
+                              
+                        </div>
+                    </div>
+               </div>
+             </div>
+
+             
+            <!-- 8 Column with Picture -->
+          <div class="container mx-auto">
+            <div class="jumbotrons">
+                <h1 class="display-4">ALA CARTE:</h1>
+                <hr class="my-4">
+            </div>
+            <div class="row mx-auto">
+                <div class="col-sm-3 mb-3 mx-auto">    
+                    <div class="card">
+                        <img src="menu\frenchfries.png"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 40.00</h5>
+                               <p class="card-text">French Fries</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3 mx-auto">    
+                    <div class="card">
+                        <img src="menu\spaghetti.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 50.00</h5>
+                               <p class="card-text">Spaghetti</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-3 mb-3 mx-auto">     
+                    <div class="card">
+                        <img src="menu\chickenskin.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Php 50.00</h5>
+                            <p class="card-text">Chicken Skin</p>
+                              
+                        </div>
+                    </div>
+               </div>
+             </div>
+
+              <!-- 9 Column with Picture -->
+          <div class="container mx-auto">
+            <div class="jumbotrons">
+                <h1 class="display-4">BEVERAGES:</h1>
+                <hr class="my-4">
+            </div>
+            <div class="row mx-auto">
+                <div class="col-sm-2 mb-2 mx-auto">    
+                    <div class="card">
+                        <img src="menu\sprite.png"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">MEAL B</h5>
+                               <p class="card-text">Sprite (8oz)/(kasalo size)</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-2 mb-2 mx-auto">    
+                    <div class="card">
+                        <img src="menu\coke.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">MEAL B</h5>
+                               <p class="card-text">Coke (8oz)/(kasalo size)</p>
+                                  
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-2 mb-2 mx-auto">     
+                    <div class="card">
+                        <img src="menu\royal.jpg"class="card-img-top mx-auto" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">MEAL B</h5>
+                            <p class="card-text">Royal (8oz)/(kasalo size)</p>
+                              
+                        </div>
+                    </div>
+               </div>
+            </div>
+
+            <div class="container mx-auto">
+                <div class="jumbotrons" style="background-color: #D2D7D3">
+                    <hr class="my-4">
+                </div>
+
+                <div class="row">
+                   <a href="user.php" class="btn btn-dark ml-auto row" style="color: white">BACK</a>
+                </div>
+            </div>
+
+            <div class="container mx-auto">
+                <div class="jumbotrons">
+                    <hr class="my-4">
+                 <h1 class="display-4 row"></h1>
+                </div>
+            
+
+
+
+</body>
+</html> 
